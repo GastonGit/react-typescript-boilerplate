93c1ec6e99e0f75d93ddaf8c16c8c8d53d2ffe62
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-10-21 13:51:32 +0200

    update app test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-test('renders learn react link', () => {
+test('renders boiled rice text', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const boilElement = screen.getByText(/Boiled rice/i);
+    expect(boilElement).toBeInTheDocument();
 });
